8de346e1a80f5e981e9b376113b1809674cc354f
Mei <1078204471@qq.com>
2022-09-05 15:29:03 +0800

    First commit

---
+++ app/src/main/java/com/example/ipadtest/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.ipadtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btn =(Button) findViewById(R.id.btn);
+        btn.setOnClickListener(this);
+        replaceFragment(new RightFragment());
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btn:
+                replaceFragment(new RightFragment2());
+                break;
+            default:
+                break;
+
+        }
+    }
+    private void replaceFragment(Fragment fragment){
+//        FragmentManager supportFragmentManager = getSupportFragmentManager();
+//        FragmentTransaction fragmentTransaction = supportFragmentManager.beginTransaction();
+//        fragmentTransaction.replace(R.id.right_layout,fragment);
+//        fragmentTransaction.addToBackStack(null);
+//        //返回到刚才的界面
+//        fragmentTransaction.commit();
+    }
+}
---
+++ app/src/main/java/com/example/ipadtest/RightFragment2.java
@@ -0,0 +1,16 @@
+package com.example.ipadtest;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class RightFragment2 extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        View view = inflater.inflate(R.layout.right_fragment2, container, false);
+        return view;
+    }
+}
